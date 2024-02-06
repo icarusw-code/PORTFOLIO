910ba03418086d9cafc8682e459d5b223039de37
SeongJin Choi <77170611+icarusw-code@users.noreply.github.com>
2024-02-05 21:46:54 -0500

    layout update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import HomeView from "./views/HomeView";
 
 const App = () => {
-    return <div>성진사이트</div>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomeView />} />
+                {/* <Route path="/about" element={<AboutView />} /> */}
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Header from '../components/Header'
+import Skip from '../components/Skip'
+import Intro from '../components/Intro'
+import Skill from '../components/Skill'
+import Site from '../components/Site'
+import Port from '../components/Port'
+import Footer from '../components/Footer'
+import Contacts from '../components/Contacts'
+import Main from '../components/Main'
+
+const HomeView = () => {
+  return (
+    <>
+        <Skip />
+        <Header />
+        <Main>
+            <Intro />
+            <Skill />
+            <Site/>
+            <Port/>
+            <Contacts />
+        </Main>
+        <Footer />
+    </>
+  )
+}
+
+export default HomeView
